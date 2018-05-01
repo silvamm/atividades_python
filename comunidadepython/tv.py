0233a82823e6567458b5789398401dc47b41a4c1
Matheus Monteiro <silvamm.matheus@gmail.com>
2018-04-30 22:39:24 -0300

    melhorando enunciado do exercício

--- comunidadepython/tv.py
+++ comunidadepython/tv.py
@@ -1,10 +1,8 @@
-#Classe TV: Faça um programa que simule 
-# um televisor criando-o como um objeto.
-#O usuário deve ser capaz de informar 
-#o número do canal e aumentar ou diminuir o volume.
-#Certifique-se de que o número do canal e 
-#o nível do volume permanecem dentro da faixas válidas.
-
+'''
+Classe TV: Faça um programa que simule um televisor criando-o como um objeto.
+O usuário deve ser capaz de informar o número do canal e aumentar ou diminuir o volume.
+Certifique-se de que o número do canal e o nível do volume permanecem dentro da faixas válidas.
+'''
 
 class TV:
 
